f51fd6d0e507d0da1128d1d8113c234eeb17a837
Veronika Teplova <teplovavo@gmail.com>
2024-09-24 19:12:33 -0700

    finished part2

--- script.js
+++ script.js
@@ -1,21 +1,38 @@
 console.log("---------part-1----------") // Part 1: Getting Started 
 
-// Step 1: Create a <main> element and store it in a variable named mainEl
+// Create a <main> element and store it in a variable named mainEl
 const mainEl = document.createElement('main');
 
-// Step 2: Set the background color
+// Set the background color
 mainEl.style.backgroundColor = 'var(--main-bg)';
 
-// Step 3: Add an <h1> 
+// Add an <h1> 
 mainEl.innerHTML = '<h1>DOM Manipulation</h1>';
 
-// Step 4: Add a class of flex-ctr to mainEl.
+// Add a class of flex-ctr to mainEl.
 mainEl.classList.add('flex-ctr');
 
-//Add mainEl to the body of the document, =>shows up on the page
+//Add mainEl to the body of the document for my practice.
 document.body.appendChild(mainEl);
 
 console.log("Script is running. I can see background color and text is centered")
 
+
+
 ///////////////////////////////////////////////////////////////////////////////////
 console.log("---------part-2----------") //Part 2: Creating a Menu Bar
+
+//Select and cache the <nav id="top-menu"> element in a variable named topMenuEl.
+const topMenuEl = document.getElementById('top-menu');
+
+// Set the height of the topMenuEl element to be 100%
+topMenuEl.style.height = '100%';
+
+// Set the background color of topMenuEl to the value stored in the --top-menu-bg 
+topMenuEl.style.backgroundColor = 'var(--top-menu-bg)';
+
+// Add a class of flex-around to topMenuEl
+topMenuEl.classList.add('flex-around');
+
+
+
